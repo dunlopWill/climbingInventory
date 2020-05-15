f1341ccac9a25cf73735f74b59c92af52c400d83
will.dunlop <62902332+will.dunlop@users.noreply.github.com>
2020-05-15 08:26:35 +0100

    Update climbingInventory.py

--- climbingInventory.py
+++ climbingInventory.py
@@ -1,8 +1,16 @@
 # climbingInventory.py
+
 # The function displayInventory() takes a dictionary of 'inventory'.
 # The function prints a list of the inventory in the format "number of items x item".
 # The function also logs the total number of items of inventory and prints this total.
 
+# The function addToInventory() takes a list of items and adds these items to the dictionary.
+# If the item key is already in the dictionary, it updates the number.
+# If the item key is not already in the dictionary, it adds the item to the dictionary.
+
+# The function removeFromInventory() takes a lsit of items and removes these from the dictionary and updates the number accordingly.
+
+# Dictionary of climbing inventory
 climbingInventory = {
 'Helmet': 1,
 '70 meter rope': 1,
@@ -19,6 +27,7 @@ climbingInventory = {
 'Hexes':3
 }
 
+#Function to display dictionary
 def displayInventory(inventory):
     print("Climbing inventory:")
     item_total = 0
@@ -27,4 +36,28 @@ def displayInventory(inventory):
         item_total+=v
     print("Total number of items: " + str(item_total))
 
+#Function to add list items to dictionary
+def addToInventory(inventory, addedItems):
+    for i in range(len(addedItems)):
+        if addedItems[i] in inventory:
+            inventory[addedItems[i]] += 1
+        else:
+            inventory[addedItems[i]] = 1
+
+#Function to remove list items from dictionary
+def removeFromInventory(inventory, lostItems):
+    for i in range(len(lostItems)):
+        if lostItems[i] in inventory:
+            inventory[lostItems[i]] -= 1
+
+shop = ['Screwgate carabiners', 'Nylon slings', 'Cord', 'Cord']
+lost = ['Hexes', 'Hexes', 'Hexes']
+
+#Testing/running of programme
+displayInventory(climbingInventory)
+
+addToInventory(climbingInventory, shop)
+displayInventory(climbingInventory)
+
+removeFromInventory(climbingInventory, lost)
 displayInventory(climbingInventory)
